fa3be7283efc894984cca46c2307801743391c2f
Yusuke Yamamoto <yusuke@mac.com>
2025-01-30 11:05:29 +0900

    コンストラクタインジェクションを適用

--- src/main/java/com/samuraism/javatraining/todo/TodoController.java
+++ src/main/java/com/samuraism/javatraining/todo/TodoController.java
@@ -1,6 +1,5 @@
 package com.samuraism.javatraining.todo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,13 +7,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-
 @SuppressWarnings("NonAsciiCharacters")
 @org.springframework.stereotype.Controller
 public class TodoController {
 
-    @Autowired
-    TaskRepository repository;
+    private final TaskRepository repository;
+
+    public TodoController(TaskRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/")
     String showTodos(Model model) {
